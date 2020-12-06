129d744db4dc8bdec7080ac6b8336877fc361379
Artur Mrozinski <amrozinski@gmail.com>
2020-12-06 12:38:47 +0000

    replace string letters with index of aplphabet

---
+++ alphabet_position.rb
@@ -0,0 +1,325 @@
+example = %{"Super napastnik Bayernu został powstrzymany przez defensywę gości. Miał udział tylko przy golu na 2:1. Poza tym to był bardzo niefortunny występ. Rzadkością w tym meczu były akcje Polaka w polu karnym. Zupełnie jak nie "Lewy" - ocenił portal ran.de, przyznając Lewandowskiemu notę 4 (w skali 1-6, gdzie "1" to klasa światowa, a "6" gra poniżej krytyki). Dla porównania Mueller i Coman otrzymali notę "1".
+
+
+
+"Defensywa Lipska miała go pod pełną kontrolą. Jedyna pozytywna rzecz dla Bayernu, że Lewandowski absorbował obrońców i dzięki temu stworzył dużo wolnego miejsca dla strzelca dwóch bramek Thomasa Muellera" - to z kolei komentarz spox.com (również nota 4).
+
+ZOBACZ WIDEO: #dziejesiewsporcie: co za gol! Ręce same składały się do oklasków
+ZAMKNIJ
+Na "czwórkę" ocenili reprezentanta Polski też eksperci "Abendzeitung". "Przy drugim golu dla Bayernu Lewandowski wyciągnął Upamecano z linii obrony i tym samym dał Muellerowi niezbędną przestrzeń. Poza tym jednak niezwykle niepozorny" - napisano w argumentacji.
+
+Mniej surowy dla "Lewego" był analityczny portal whoscored.com. Za występ przeciwko Lipskowi kapitan reprezentacji Polski dostał ocenę 6.6 (w skali 1-10, gdzie "10" to najwyższa nota). Wyższe noty w Bayernie otrzymali tylko Mueller (8.0), Coman (7.9) i Alaba (6.7).
+
+
+ADVERTISING
+
+
+
+Zobacz:
+Bundesliga: Borussia Dortmund nie goni liderów. Remis wicemistrza Niemiec
+Transfery. Prezes Barcelony chciał sprzedać Messiego. Teraz się tłumaczy
+
+Oglądaj Bundesligę NA ŻYWO w WP Pilot!
+
+Polub Piłkę Nożną na Facebooku
+
+ Udostępnij  Tweetnij  Skomentuj 5
+
+WP SportoweFakty
+POLECANE
+
+Nowe informacje o Robercie Lewandowskim. Decyzja w drugiej połowie tygodnia
+
+Masa pasożytów z Ciebie wylezie, jeśli rano do...
+
+Bundesliga. "To, co pokazuje na boisku, jest bardzo dojrzałe". Trener Bayernu chwali 17-letniego piłkarza
+
+Problem z żylakami może zniknąć, wystarczy rano...
+
+Bundesliga. Znamy składy na mecz Bayern Monachium - RB Lipsk. Robert Lewandowski uporał się z problemami
+
+"Dlaczego pominięto Lewandowskiego?" Fani Bayernu Monachium zdenerwowani
+
+Komentarze (5):
+Napisz komentarz...
+Zaloguj się do serwisu aby dodać komentarz. Nie masz jeszcze konta? Zarejestruj się!
+ Bratomir Almanowski 45 min
+No to Pan Kucharski potrafił jak się okazuje, sprzedać drewno za cenę złota...
+3  5
+ Tolek12 to Seba jest miękką fają 49 min
+Owoc miłości kozy jeszcze buszuje po necie hehehe :D
+1  3
+
+ Tolek12 to Seba jest miękką fają 55 min
+@Poot nie tylko Bayern i Woodenowski grał 50 spotkań w zeszłym sezonie, pozostałe drużyny też. Sukces Bayernu i to potwierdza Kloop w jednej ze swoich wypowiedzi prasowych ukryty był wCzytaj całość
+3  3
+ Poot 1 h 16 min
+@Tolek12 to Seba jest miękką fają: hmmm... bez formy, bez pomysłu i w 10-ciu meczach BuLi strzelił 12 bramek. porównujesz "kilka meczy" do całego sezonu??? Ponad 50 meczyCzytaj całość
+1  2
+ Tolek12 to Seba jest miękką fają 1 h 51 min
+Jak zwykle zaklinanie rzeczywistości przez SF. "Właśnie jak Drewandowski" i to od kilku meczy. Bez formy, bez pomysłu i z kolejnym argumentem że sezon 19/20 był dziełemCzytaj całość
+8  3
+RB Lipsk Piłka nożna w Niemczech Piłka w Europie Allianz Arena w Monachium Monachium Niemcy Bundesliga Piłka nożna Bayern Monachium Robert Lewandowski Thomas Mueller
+NAJNOWSZEWyniki meczów
+18 min
+PIŁKA NOŻNA
+Rosja. Trener w superlatywach o postępach Sebastiana Szymańskiego. "Uwielbiam jego styl"
+
+34 min
+PIŁKA NOŻNA
+Premier League. "Nie odpowiada za stracone gole, ale...". Media oceniły Łukasza Fabiańskiego
+
+51 min
+PIŁKA NOŻNA
+PKO Ekstraklasa. Lech Poznań pragnie zwiększyć pole manewru. Na celowniku czeski stoper
+
+1 h 2 min
+PIŁKA NOŻNA
+Fortuna I liga. Bruk-Bet Termalica Nieciecza - ŁKS Łódź 2:0 (galeria)
+
+
+1 h 5 min
+PIŁKA NOŻNA
+PKO Ekstraklasa. Legia - Lechia. Tomas Pekhart zrobił to perfekcyjnie. Zobacz bramkę (wideo)
+
+1 h 8 min
+PIŁKA NOŻNA
+Serie A: Inter Mediolan odczarował Bolognę FC. Achraf Hakimi pokonał dwa razy Łukasza Skorupskiego
+
+1 h 10 min
+PIŁKA NOŻNA
+Bundesliga. Manuel Neuer chwali sobie współpracę z Robertem Lewandowskim. "Jest unikalny"
+
+1 h 13 min
+PIŁKA NOŻNA
+Bundesliga. "To, co pokazuje na boisku, jest bardzo dojrzałe". Trener Bayernu chwali 17-letniego piłkarza
+
+1 h 18 min
+PIŁKA NOŻNA
+Premier League. Chelsea - Leeds United. Wygrana i lider dla "The Blues" po trudnym meczu
+
+1 h 20 min
+PIŁKA NOŻNA
+PKO Ekstraklasa. Legia - Lechia. Ofensywne usposobienie gdańszczan nic nie dało. Wygrała warszawska konsekwencja
+
+1 h 24 min
+PIŁKA NOŻNA
+Ligue 1: Paris Saint-Germain wróciło do zwyciężania. 100. gol Kyliana Mbappe
+
+1 h 26 min
+PIŁKA NOŻNA
+La Liga. Cadiz - Barcelona: czwarta porażka zespołu Ronalda Koemana. Kuriozalny gol dla gospodarzy
+
+1 h 43 min
+PIŁKA NOŻNA
+Bandycki faul na Bartoszu Kapustce. Cud, że nic mu się nie stało!
+
+1 h 46 min
+PIŁKA NOŻNA
+Transfery. Giganci walczą o pomocnika Brighton and Hove Albion. Jakub Moder może stracić konkurenta
+
+1 h 50 min
+PIŁKA NOŻNA
+Premier League. Massimiliano Allegri podsyca spekulacje. "Chciałbym doświadczyć pracy w Anglii"
+
+2 h 13 min
+PIŁKA NOŻNA
+PKO Ekstraklasa: Jagiellonia - Warta. Piotr Tworek rozczarowany wynikiem. "Kolejna końcówka jest dla nas dramatem"
+
+2 h 16 min
+FUTSAL
+Futsal: kolejna zmiana lidera
+
+2 h 18 min
+PIŁKA NOŻNA
+Anglia. Kamil Grosicki z szansą na pierwszy występ w Premier League
+
+2 h 25 min
+PIŁKA NOŻNA
+PKO Ekstraklasa: Legia Warszawa ponownie na czele ligi (tabela)
+
+2 h 26 min
+PIŁKA NOŻNA
+Bundesliga. Bayern - Lipsk. "Był niezwykle niepozorny". Fatalne noty Roberta Lewandowskiego
+
+2 h 28 min
+PIŁKA NOŻNA
+PKO Ekstraklasa. Legia - Lechia. Hit rozczarował, trzy punkty zostały jednak w Warszawie
+
+2 h 37 min
+PIŁKA NOŻNA
+Serie A. Wojciech Szczęsny bohaterem Juventusu. Włosi zachwycają się jedną interwencją
+
+2 h 43 min
+PIŁKA NOŻNA
+Kontrowersyjny wybór Gabriela Paulisty. Piłkarz postanowił zmienić barwy narodowe
+
+3 h 11 min
+PIŁKA NOŻNA
+Major League Soccer. Adam Buksa przed wielką szansą. "To duża odpowiedzialność"
+
+3 h 24 min
+PIŁKA NOŻNA - Relacja Live
+ Montpellier vs Paris Saint-Germain
+3 h 24 min
+PIŁKA NOŻNA - Relacja Live
+ Cadiz CF vs FC Barcelona
+3 h 24 min
+PIŁKA NOŻNA - Relacja Live
+ Chelsea FC vs Leeds United
+3 h 46 min
+PIŁKA NOŻNA
+Premier League. Manchester United znów odrobił straty z nawiązką. Decydujące wejście rezerwowych
+
+3 h 55 min
+PIŁKA NOŻNA
+PKO Ekstraklasa. Śląsk - Raków. Vitezslav Lavicka postawił na nowych piłkarzy. Szromnik i Janasik się odpłacili
+
+3 h 58 min
+PIŁKA NOŻNA
+Bundesliga. To była bijatyka! Bayern remisuje z RB Lipsk 3:3
+
+3 h 59 min
+PIŁKA NOŻNA
+Fortuna I liga. Widzew Łódź - Korona Kielce. Mecz pod znakiem zapytania. Powodem koronawirus
+
+Wczoraj 20:13
+PIŁKA NOŻNA
+PKO Ekstraklasa. Śląsk - Raków. Marek Papszun: Jesteśmy źli, rozczarowani i wkurzeni
+
+Wczoraj 20:06
+PIŁKA NOŻNA
+Holandia. Szok w Eredivisie. Szkoleniowiec pożegnał się z AZ Alkmaar w niecodziennych okolicznościach
+
+Wczoraj 20:05
+PIŁKA NOŻNA
+Serie A: Juventus FC uratował się w derbach. Dramat Torino FC
+
+Wczoraj 19:48
+PIŁKA NOŻNA
+PKO Ekstraklasa: Jagiellonia - Warta. Fiodor Cernych zagrał tuż po śmierci ojca. "Pokazał wielkie serce"
+
+Wczoraj 20:00
+PIŁKA NOŻNA - Relacja Live
+ Legia Warszawa vs Lechia Gdańsk
+Wczoraj 19:44
+PIŁKA NOŻNA
+Transfery. Prezes Barcelony chciał sprzedać Messiego. Teraz się tłumaczy
+
+Wczoraj 19:37
+PIŁKA NOŻNA
+Premier League. Paul Merson nie oszczędza Pierre'a-Emericka Aubameyanga. "Jest okropny"
+
+Wczoraj 19:23
+PIŁKA NOŻNA
+PKO Ekstraklasa. Śląsk - Raków. Przerwana seria lidera. Wrocławianie wyrwali 3 punkty po golu Pawłowskiego
+
+Wczoraj 19:11
+PIŁKA NOŻNA
+"Zwykła ludzka złość". Cracovia zabrała głos po derbach Krakowa
+
+Pokaż więcej
+DZIŚ W TV
+22:25
+SportklubTRWA Sportklub -
+Koszykówka mężczyzn: Liga hiszpańska - mecz: Club Joventut Badalona - Casademont Zaragoza
+23:00
+Eleven Sports 2TRWA Eleven Sports 2 -
+Piłka nożna: Liga włoska - mecz: Inter Mediolan - Bologna FC
+23:15
+TVP SportTRWA TVP Sport -
+Skoki narciarskie: Zawody Pucharu Świata w Niżnym Tagile
+23:35
+nSPORTTRWA nSPORT -
+Piłka nożna: Liga hiszpańska - mecz: Cadiz CF - FC Barcelona
+23:35
+Eurosport 1TRWA Eurosport 1 -
+Skoki narciarskie: Zawody Pucharu Świata w Niżnym Tagile
+23:35
+Eurosport 1 HDTRWA Eurosport 1 HD -
+Skoki narciarskie: Zawody Pucharu Świata w Niżnym Tagile
+23:50
+FightklubTRWA Fightklub - magazyn sportów walki
+The Fighter
+00:00
+Eleven Sports 1TRWA Eleven Sports 1 -
+Formuła 1: Grand Prix Sakhiru - sesja kwalifikacyjna
+00:00
+Eurosport 2 HDTRWA Eurosport 2 HD -
+Narciarstwo alpejskie: Zawody Pucharu Świata w Santa Caterina Valfurva - 2. przejazd slalomu giganta mężczyzn
+00:00
+Polsat SportTRWA Polsat Sport -
+Siatkówka mężczyzn: PlusLiga - mecz: Grupa Azoty ZAKSA Kędzierzyn-Koźle - PGE Skra Bełchatów
+00:00
+Polsat Sport NewsTRWA Polsat Sport News -
+Biatlon: Zawody Pucharu Świata w Kontiolahti - sztafeta kobiet
+00:00
+Eurosport 2TRWA Eurosport 2 -
+Narciarstwo alpejskie: Zawody Pucharu Świata w Santa Caterina Valfurva - 2. przejazd slalomu giganta mężczyzn
+00:00
+Polsat Sport ExtraTRWA Polsat Sport Extra -
+Siatkówka mężczyzn: Liga włoska - mecz: Consar Rawenna - Cucine Lube Civitanova
+00:00
+Extreme SportsTRWA Extreme Sports - magazyn sportowy
+Action Sports World
+00:00
+Polsat Sport HDTRWA Polsat Sport HD -
+Siatkówka mężczyzn: PlusLiga - mecz: Grupa Azoty ZAKSA Kędzierzyn-Koźle - PGE Skra Bełchatów
+00:10
+CANAL+ Sport HDTRWA CANAL+ Sport HD -
+Piłka nożna: Liga francuska - mecz: Montpellier HSC - Paris Saint-Germain
+00:30
+Eurosport 1Eurosport 1 -
+Narciarstwo alpejskie: Zawody Pucharu Świata w Sankt Moritz - supergigant kobiet
+00:30
+Eurosport 1 HDEurosport 1 HD -
+Narciarstwo alpejskie: Zawody Pucharu Świata w Sankt Moritz - supergigant kobiet
+00:30
+Eurosport 2Eurosport 2 -
+Narciarstwo dowolne: Zawody Pucharu Świata w Ruce
+00:30
+Eurosport 2 HDEurosport 2 HD -
+Narciarstwo dowolne: Zawody Pucharu Świata w Ruce
+źródło: tv.wp.pl
+
+
+Kolejne karty SIM nawet za 50% ceny!
+
+
+ Żużel. To ona skradła serce Jacka Holdera. Poznajcie Jessikę
+ Marcin Najman odpowiada Przemysławowi Salecie. "Zaczął oddychać rękawami i się poddał"
+ Oryginalni goście na treningu Justyny Kowalczyk. Była narciarka pokazała zdjęcie
+ Piękna zawodniczka zadziwiła świat. Nakręciła film, na którym surfuje nago
+ Dziennikarz mógłby zawstydzić piłkarzy z PKO Ekstraklasy. Ależ bramka!
+ Tenis. Rafael Nadal odznaczony. Otrzymał order od władz Madrytu
+Więcej artykułów
+NAJNOWSZE WIDEO
+ Liga Mistrzów. Doskonałe interwencje Manuela Neuera
+ #dziejesiewsporcie: co za gol! Ręce same składały się do oklasków
+ Liga Mistrzów. Zinedine Zidane straci pracę? Trener Realu Madryt zabrał głos na konferencji
+ Liga Mistrzów. Realowi Madryt grozi spadek do Ligi Europy. "Królewscy znajdują się nad przepaścią"
+ Liga Mistrzów. Szachtar Donieck ma patent na Real Madryt. "Zdobyli 7 punktów, w tym 6 w meczach z Królewskimi"
+Więcej wideo}
+
+
+# def alphabet_position(text)
+#   text.gsub(/[^a-z]/i, '').chars.map{ |c| c.downcase.ord - 96 }.join(' ')
+# end
+
+def alphabet_position(text)
+      text
+      .downcase
+      .chars
+      .map(&:ord)
+      .map {|ord| ord - 96}
+      .join(' ')
+end
+
+# def alphabet_position(text)
+#   alph = ('a'..'z').to_a
+#   str = ""
+#   text.downcase.scan(/[a-z]/).each {|i| str << (alph.find_index(i).to_i + 1).to_s + " " }
+#   p str.strip
+# end
+
+1000.times {puts alphabet_position(example)}
